e64c5a4883ce638273a3403ecd8ebeca9d3f148e
Tyler Lynch <tylerlynch@tylers-mbp.home>
2019-07-13 12:40:21 -0400

    m

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Row className="App-Header">
       <Col>
-        <Row>         
+        <Row>          
           <Col md="12">
             <TopNavBar />
           </Col>
