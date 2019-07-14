1d8179c9fd32a6319266e4a431a971002a43382b
Tyler Lynch <tylerlynch01@gmail.com>
2019-07-14 11:55:46 -0400

    adjust layout, change to document editor

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React from 'react';
+import React from "react";
 import './App.css';
 import TopNavBar from './components/TopNavigationBar/TopNavBar';
 import SideNavBar from './components/SideNavigationBar/SideNavBar';
 import { Row, Col } from 'reactstrap';
-import NoteEditer from './components/NoteEditer/NoteEditer';
+import NoteEditor from './components/NoteEditor/NoteEditor';
 
-function App() {
-  return (
-    <Row className="App-Header">
-      <Col>
-        <Row>          
-          <Col md="12">
-            <TopNavBar />
-          </Col>
-        </Row>
-        <Row>
-          <Col md="1">
-            <SideNavBar />
-          </Col>
-          <Col md="10">
-            <NoteEditer />
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			sidebarOpen: true
+		};
+		this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+	}
+
+	onSetSidebarOpen(open) {
+		this.setState({ sidebarOpen: open });
+	}
+	render() {
+		return (
+			<Row className="App-Header">
+				<Col md="10">
+					<Row>
+						<Col>
+							<TopNavBar />
+						</Col>
+					</Row>
+					<Row className="bodyForm">
+						<Col md="1">
+							<SideNavBar />
+						</Col>
+						<Col md="11">
+							<NoteEditor />
+						</Col>
+					</Row>
+				</Col>
+			</Row>
+		);
+	}
 }
 
 
